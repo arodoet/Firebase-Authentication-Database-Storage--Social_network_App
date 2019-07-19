748edd3a64b0887b57c1a48afafebccfc2629ccf
arodoet <teodoraknezevic@gmail.com>
2019-07-19 12:14:37 +0200

    Initial Commit

---
+++ social_network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  social_network
+//
+//  Created by Teodora Knezevic on 7/19/19.
+//  Copyright © 2019 Teodora Knezevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
